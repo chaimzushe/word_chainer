fd416147cc7c8e0e16b8b93c8cebca6190d60664
Chaim Zushe Wilmowsky <chaimzushe@gmail.com>
2017-11-06 16:15:57 -0500

    fixed bug in finding adjacent words, plus added a tracking system

--- word_chain.rb
+++ word_chain.rb
@@ -7,7 +7,7 @@ class WordChainer
 # src => duck
   def run(src, target)
     @current_words = [src]
-    @all_seen_words = [src]
+    @all_seen_words = { src => nil }
 
     until @current_words.empty?
       explore_current_words
@@ -28,11 +28,11 @@ class WordChainer
         adjacent_words(word).each do |adjacent_word|
           next if @all_seen_words.include?(adjacent_word)
           new_current_words << adjacent_word
-           @all_seen_words << adjacent_word
+          @all_seen_words[adjacent_word] = word
         end
       end
-      p new_current_words
-      @current_words = new_current_words
+      new_current_words.each { |word| puts "#{word} came from, #{@all_seen_words[word]}" }
+      @current_words = new_current_words # => all the adjacent words are now inside currrent_words
     end
 
     def adjacent_words(word)
@@ -44,9 +44,9 @@ class WordChainer
     def one_letter_off(word1, word2)
       return false if word1 == word2 || word1.size != word2.size
       diffrent = 0;
-      chars_seen = Hash.new(false)
-      word1.chars.each { |chr| chars_seen[chr] = true }
-      word2.chars.each {|chr| diffrent += 1 if chars_seen[chr] == false}
+      chars_seen = Hash.new(0)
+      word1.chars.each { |chr| chars_seen[chr] += 1 }
+      word2.chars.each {|chr| diffrent += 1 if word2.count(chr) != chars_seen[chr] }
       diffrent == 1
     end
 end
